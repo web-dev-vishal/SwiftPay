f532e9394c5e0fd8a4f03e658468a1b85997f9d6
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-31 01:46:29 +0530

    code added in jestconfig file Done

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    testEnvironment: 'node',
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+      'src/**/*.js',
+      '!src/**/index.js',
+      '!**/node_modules/**',
+    ],
+    testMatch: [
+      '**/tests/**/*.test.js',
+    ],
+    verbose: true,
+    forceExit: true,
+    clearMocks: true,
+    resetMocks: true,
+    restoreMocks: true,
+  };
